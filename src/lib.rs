fe942d04059129e2a0dadb2c6039782212600852
nihilityer <hch2796945700@outlook.com>
2023-10-30 21:24:34 +0800

    init nihility-common

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+pub mod manipulate {
+    tonic::include_proto!("manipulate");
+}
+
+pub mod instruct {
+    tonic::include_proto!("instruct");
+}
+
+pub mod module_info {
+    tonic::include_proto!("module_info");
+}
